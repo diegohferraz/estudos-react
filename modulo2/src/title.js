90bae459f2653e1b89f3e29c48c7d9d9cb8a7661
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2019-06-11 10:35:29 -0300

    default props example

--- modulo2/src/title.js
+++ modulo2/src/title.js
@@ -3,9 +3,15 @@
 import React from 'react'
 
 const Title = React.createClass({
+    getDefaultProps: function () {
+        return {
+            name: 'Desconhecido',
+            lastName: 'Sem Sobrenome'
+        }
+    },
     render: function () {
         return (
-            <h1>Olá {this.props.name}</h1>
+            <h1>Olá {this.props.name} {this.props.lastName}</h1>
         )
     }
 })
