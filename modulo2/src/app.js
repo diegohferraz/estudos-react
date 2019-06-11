e94efb62a6203389ccba55cef545ec305fd65d80
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2019-06-11 09:17:12 -0300

    configure hot loader

--- modulo2/src/app.js
+++ modulo2/src/app.js
@@ -2,10 +2,10 @@
 
 import React from 'react'
 
-const Title = React.createClass({
+const App = React.createClass({
     render: function () {
         return <h1>Titulo</h1>
     }
 })
 
-export default Title
+export default App
--- modulo2/src/index.js
+++ modulo2/src/index.js
@@ -4,11 +4,22 @@ import React from 'react'
 import { render } from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 
-import Title from './app'
+import App from './app'
 
-render(
-    <AppContainer>
-        <Title />
-    </AppContainer>,
-    document.querySelector('[data-js="app"]')
-)
+const renderApp = (NextApp) => {
+    render(
+        <AppContainer>
+            <NextApp />
+        </AppContainer>,
+        document.querySelector('[data-js="app"]')
+    )
+}
+
+renderApp(App)
+
+if (module.hot) {
+    module.hot.accept('./app', () => {
+        const NextApp = require('./app').default
+        renderApp(NextApp)
+    })
+}
