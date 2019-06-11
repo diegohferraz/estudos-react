e49fb548db210adf6507be83bbbf86cbc650fa3d
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2019-06-11 10:58:40 -0300

    pure function obj

--- modulo2/src/title.js
+++ modulo2/src/title.js
@@ -2,18 +2,27 @@
 
 import React from 'react'
 
-const Title = React.createClass({
-    getDefaultProps: function () {
-        return {
-            name: 'Desconhecido',
-            lastName: 'Sem Sobrenome'
-        }
-    },
-    render: function () {
-        return (
-            <h1>Olá {this.props.name} {this.props.lastName} - {this.props.age - 2 } Anos</h1>
-        )
-    }
-})
+const Title = (props) => (
+    <h1>Olá {`${props.name} ${props.lastName} - ${props.age} Anos!`}</h1>
+)
+
+Title.defaultProps = {
+    name: 'Desconhecido',
+    lastName: 'Sem Sobrenome'
+}
+
+// const Title = React.createClass({
+//     getDefaultProps: function () {
+//         return {
+//             name: 'Desconhecido',
+//             lastName: 'Sem Sobrenome'
+//         }
+//     },
+//     render: function () {
+//         return (
+//             <h1>Olá {this.props.name} {this.props.lastName} - {this.props.age - 2 } Anos</h1>
+//         )
+//     }
+// })
 
 export default Title
