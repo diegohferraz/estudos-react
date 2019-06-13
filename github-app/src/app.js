e63b067297c5e20a517e7f18a6ccd81a76360d23
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2019-06-13 10:03:48 -0300

    basic app markup

--- github-app/src/app.js
+++ github-app/src/app.js
@@ -5,7 +5,49 @@ import React from 'react'
 class App extends React.Component {
   render () {
     return (
-      <div>My Apper</div>
+      <div className='app'>
+        <div className='search'>
+          <input
+            type='search'
+            placeholder='digite o nome do usuario' />
+        </div>
+        <div className='user-info'>
+          <img src='https://avatars0.githubusercontent.com/u/3427663?v=4' />
+          <h1>
+            <a href='https://github.com/diegohferraz'>Diego Ferraz</a>
+          </h1>
+
+          <ul className='repos-info'>
+            <li>- Repositorios: 122</li>
+            <li>- Seguidores: 10</li>
+            <li>- Seguindo: 122</li>
+          </ul>
+
+          <div className='actions'>
+            <button>Ver repositórios</button>
+            <button>Ver favoritos</button>
+          </div>
+
+          <div className='repos'>
+            <h2>Repositorios</h2>
+            <ul>
+              <li>
+                <a href='#'>Nome do Repositorio</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className='starred'>
+            <h2>Favoritos</h2>
+            <ul>
+              <li>
+                <a href='#'>Nome do Repositorio</a>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
     )
   }
 }
